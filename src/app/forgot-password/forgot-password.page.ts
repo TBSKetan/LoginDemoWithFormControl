40cfbf9bf32c2358022ae72a7ed5b5d303a6521c
Ketan Nakum <tbs.ketan@gmail.com>
2022-04-01 19:41:19 +0530

    impliment validation

--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CONSTANT } from '../utility/constantLogin';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
@@ -8,10 +8,28 @@ import { CONSTANT } from '../utility/constantLogin';
 })
 export class ForgotPasswordPage implements OnInit {
   public stringConst = CONSTANT;
-
-  constructor() { }
-
-  ngOnInit() {
+  ionicForm: FormGroup;
+  isSubmitted = false;
+  constructor(public formBuilder: FormBuilder) {
+    this.ionicForm = this.formBuilder.group({
+           email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    });
   }
 
+  ngOnInit() {}
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      //console.log('Please provide all the required values!');
+      return false;
+    } else {
+      //console.log(this.ionicForm.value);
+      return this.ionicForm.value;
+    }
+  }
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
 }
